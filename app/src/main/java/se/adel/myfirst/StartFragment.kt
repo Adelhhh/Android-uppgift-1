bfb7b96b07eb0c18b44f9dfb3b714a7cf0d508df
Adel <adelhassanpour.ux@gmail.com>
2024-02-04 18:58:08 +0100

    Initial commit

---
+++ app/src/main/java/se/adel/myfirst/StartFragment.kt
@@ -0,0 +1,30 @@
+package se.adel.myfirst
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.StartView).text = "Welcome"
+
+        view.findViewById<Button>(R.id.ButtonView).setOnClickListener {
+            view.findViewById<TextView>(R.id.StartView).text = "Goodbye"
+        }
+    }
+}
